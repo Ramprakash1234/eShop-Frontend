df215d5e287b8cc58943ef3476ae1d523ea80ba1
Ramprakash <srp_india@yahoo.com>
2021-06-25 16:42:31 +0530

    removing state from app.js

--- src/App.js
+++ src/App.js
@@ -11,42 +11,15 @@ import { useState } from "react";
 import {connect} from 'react-redux'
 import {bindActionCreators} from 'redux'
 const App=(props) => {
-  //const [cart,updateCart]=useState([]);
   const [searchPattern,changeSearchPattern]=useState('');
   const [sortType,changeSortType]=useState('none')
-  // console.log(searchPattern)
   const filterProducts = (e) =>{
     changeSearchPattern(e.target.value)
   }
   const sortProducts=(sortType)=>{
     changeSortType(sortType);
   }
-  const modifyCart =async(prod,op)=>{
-    //console.log(prod)
-    const isAvailable=await props.cart.find((cartItem)=>cartItem.id==prod._id);
-    //console.log(isAvailable)
-    if(isAvailable){
-      
-      let newCart=await props.cart.map((cartItem)=>{
-        if(cartItem.id==prod._id && op=='add'){
-          return {...cartItem,qty:cartItem.qty+1}
-        }else if(cartItem.id==prod._id && op=='sub'){
-          return {...cartItem,qty:cartItem.qty-1}
-        }else if(cartItem.id==prod._id && op=='remove'){
-          return 'empty'
-        }
-        return {...cartItem}
-      })
-      // console.log(newCart)
-      newCart=newCart.filter((item)=>item!='empty');
-      updateCart([...newCart])
-    }else{
-      //console.log(prod)
-      console.log('going to idspatch')
-      props.dispatch(actions.newAction(prod))
-      //updateCart([...cart,{id:prod._id,title:prod.title,description:prod.description,image:prod.image,price:prod.price,qty:1}]);
-    }
-  }
+  
   return (
     <Router>
       {console.log(props)}
